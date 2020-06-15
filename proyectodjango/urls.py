bfc6b9596fdca6f47e635b49b4e4d58f73887fee
AnnieLan <183465@ids.upchiapas.edu.mx>
2020-06-15 15:07:09 -0500

    add

--- proyectodjango/urls.py
+++ proyectodjango/urls.py
@@ -37,6 +37,7 @@ router.register(r'users',UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^api/v1/login', include('Login.urls')),
     re_path(r'^', include(router.urls)),
     path('Swagger/', schema_view),
 ]
